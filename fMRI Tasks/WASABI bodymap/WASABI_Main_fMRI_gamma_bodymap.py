74eb320ccc60b0d56ff54edb9a557bde03dd5c3f
Michael-Sun <msun@dartmouth.edu>
2021-05-13 22:07:19 -0400

    Collected sub-00003_ses-04 bodymapST2

--- fMRI Tasks/WASABI bodymap/WASABI_Main_fMRI_gamma_bodymap.py
+++ fMRI Tasks/WASABI bodymap/WASABI_Main_fMRI_gamma_bodymap.py
@@ -83,7 +83,7 @@ __status__ = "Production"
 0b. Beta-Testing Togglers
 Set to 1 during development, 0 during production
 """
-debug = 1
+debug = 0
 autorespond = 0
 # Device togglers
 biopac_exists = 1
