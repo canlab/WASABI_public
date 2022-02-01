a272729093ecff03f0cce5a973ff4d2414f970b1
Michael-Sun <msun@dartmouth.edu>
2022-02-01 11:43:41 -0500

    SUB 7, SES 9, BODYMAPst2

--- fMRI Tasks/WASABI Task-T1/WASABI_Nback.py
+++ fMRI Tasks/WASABI Task-T1/WASABI_Nback.py
@@ -5,7 +5,8 @@ This experiment was created using PsychoPy3 Experiment Builder (v2020.2.10),
     on January 28, 2021, at 23:04
 If you publish work using this script the most relevant publication is:5555
 
-    Peirce J, Gray JR, Simpson S, MacAskill M, Höchenberger R, Sogo H, Kastman E, Lindeløv JK. (2019) 
+    Peirce J, Gray JR, Simpson S, MacAskill M, Höche
+    nberger R, Sogo H, Kastman E, Lindeløv JK. (2019) 
         PsychoPy2: Experiments in behavior made easy Behav Res 51: 195. 
         https://doi.org/10.3758/s13428-018-01193-y
         
