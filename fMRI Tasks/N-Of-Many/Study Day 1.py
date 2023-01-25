65fca1768b044620188cd3b37267089b0d94b64d
Michael-Sun <msun@dartmouth.edu>
2023-01-24 20:35:33 -0500

    Collected sub-SID002448_ses-01 bodycalibration and T1

--- fMRI Tasks/N-Of-Many/Study Day 1.py
+++ fMRI Tasks/N-Of-Many/Study Day 1.py
@@ -1,4 +1,4 @@
-3241# == WASABI MedMap==
+# == WASABI MedMap==
 import os
 
 os.system('python "C:\\Users\\Dartmouth\\Documents\\GitHub\\canlab\\WASABI_public\\fMRI Tasks\\N-Of-Many\\WASABI bodyCalibration\\WASABI_bodyCalibration_1.0.4.py"')
