b63ea25b11291fcdca62bb3cd89dac17ffa63c31
Michael-Sun <msun@dartmouth.edu>
2023-03-12 12:34:30 -0400

    Updated Paradigm versions

--- fMRI Tasks/N-Of-Many/Study Day 1.py
+++ fMRI Tasks/N-Of-Many/Study Day 1.py
@@ -2,4 +2,3 @@
 import os
 
 os.system('python "C:\\Users\\Dartmouth\\Documents\\GitHub\\canlab\\WASABI_public\\fMRI Tasks\\N-Of-Many\\WASABI bodyCalibration\\WASABI_bodyCalibration_1.0.4.py"')
-5555
