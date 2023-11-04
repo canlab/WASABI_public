04dcca2d6bcb7040aa02b166815d55ac976512c0
Michael Sun <msun@dartmouth.edu>
2023-11-04 12:08:13 -0400

    Updates to WASABI_generate_firstlevel

--- scripts/fMRI/firstlvl/WASABI_generate_firstlevel.m
+++ scripts/fMRI/firstlvl/WASABI_generate_firstlevel.m
@@ -35,39 +35,68 @@
 % % FUNCS.runs=runs;
 % FUNCS.funcs;
 
+fmriprep_derivdir='\\dartfs-hpc\rc\lab\C\CANlab\labdata\data\WASABI\derivatives'
+bidsroot='\\dartfs-hpc\rc\lab\C\CANlab\labdata\data\WASABI\1080_wasabi'
 
+function WASABI_genfirstlvlDSGNs(fmriprep_derivdir, bidsroot, varargin)
+    % Step 1: Put in a firstlvl_derivdir full path.
+    % Step 2: 
 
-function WASABI_genfirstlvlDSGNs(firstlvl_derivdir, bidsroot)
-    if ~exist(firstlvl_derivdir, 'dir')
-        fmriprepdir=fullfile(fileparts(firstlvl_derivdir), 'fmriprep')
-        if ~exist(fmriprepdir, 'dir')
-            error('No fmriprep directory named fmriprep...Cannot create firstlvl_derivdir. Run canlab_prep_bidsdir() first to create a first level directory.')
-        else
-            canlab_prep_bidsdir(fmriprepdir, 'datatype', 'func', 'outdir', firstlvl_derivdir)
+    if ~exist(fmriprep_derivdir, 'dir')
+        error('No fmriprep directory named fmriprep...Cannot find or create firstlvl_derivdir. Run canlab_prep_bidsdir() first to create a first level directory.');
+    else
+        firstlvl_derivdir = fullfile(fmriprep_derivdir, 'canlab_firstlvl')
+        if ~exist(firstlvl_derivdir, 'dir')
+            warning('no canlab_firstlvl directory found. Creating one from fmriprep derivatives.')
+            canlab_prep_bidsdir(fmriprepdir, 'datatype', 'func', 'outdir', firstlvl_derivdir);
         end
     end
 
+    % Parse varargin for task, space, and subs
+    p = inputParser;
+    addOptional(p, 'task', '', @ischar);
+    addOptional(p, 'space', 'MNI152NLin2009cAsym', @ischar);
+    addOptional(p, 'subs', {}, @iscell);
+    parse(p, varargin{:});
 
-    varargin=[task, subs]
+    task = p.Results.task;
+    space = p.Results.space;
+    subs = p.Results.subs;
 
-    % Defaults
-    tasks=dir(fullfile(bidsroot, 'task-*'));
-    tasks=extractBetween({tasks.name}, 'task-', '_');
+    % Defaults for tasks
+    if isempty(task)
+        tasks = dir(fullfile(bidsroot, 'task-*'));
+        tasks = extractBetween({tasks.name}, 'task-', '_');
+        if isempty(tasks)
+            error('No tasks identified.');
+        end
+    else
+        tasks = {task}; % If task is specified, use it directly
+    end
 
-    % if tasks not defined in varargin...perhaps intelligently group the
-    % tasks?
+    % Default for subs if not provided
+    if isempty(subs)
+        subs = canlab_list_subjects(fullfile(firstlvl_derivdir), 'sub-*');
+    end
 
-    space='MNI152NLin2009cAsym';
-    subs=canlab_list_subjects(fullfile(firstlvl_derivdir), 'sub-*');
+    % Define the list of allowed templateflow standard spaces
+    allowedSpaces = {'MNI152Lin', 'MNI152NLin2009cAsym', 'MNI152NLin6Asym', ...
+                     'MNI152NLin6Sym', 'MNIInfant', 'MNIPediatricAsym', ...
+                      'NKI', 'OASIS30ANTs', 'PNC', 'fsLR', 'fsaverage', ...
+                      'anat', 'T1w', 'run', 'func', 'sbref', 'fsnative', ...
+                      'template', 'fsnative', 'fsaverage6', 'fsaverage5'}; 
+                        % Add all required standard spaces here
+    
+    % Check if provided space is one of the allowed spaces
+    if ~ismember(space, allowedSpaces)
+        error('The specified space "%s" is not one of the allowed templateflow standard spaces.', space);
+    end
 
-    % cd(fullfile(firstlvl_derivdir))
-    for s = 1:numel(subs)
-        % cd(fullfile(firstlvl_derivdir,subs{s}))
-        
-        sess=canlab_list_subjects(fullfile(firstlvl_derivdir, subs{s}), 'ses-*');
-        for i=1:numel(sess)
-            % Try not to cd, instead directly access the folder.
-            disp(['Processing images for ', subs{s}, ' ', sess{i}]);
+
+    for sub = 1:numel(subs)
+        sessions=canlab_list_subjects(fullfile(firstlvl_derivdir, subs{sub}), 'ses-*');
+        for ses=1:numel(sessions)
+            disp(['Processing images for ', subs{sub}, ' ', sessions{ses}]);
             
             % List out all of the functional scans in the MNI Space for each task
             
@@ -80,20 +109,27 @@ function WASABI_genfirstlvlDSGNs(firstlvl_derivdir, bidsroot)
             % hyperalignment_funcs = dir('*task-hyperalignment*MNI*preproc_bold.nii.gz');     % List out all of the functional scans in MNI space
             
             % For each subject: All funcs in the session:
-            ses_funcs = dir(fullfile(pwd, '**', ['*' space,'*desc-preproc_bold.nii.gz']));
+            ses_funcs = filenames(fullfile(firstlvl_derivdir, subs{sub}, sessions{ses}, '**', ['*' space, '*desc-preproc_bold.nii.gz']));
             
+            
+            
+            ses_funcs(find(contains(ses_funcs, task)))
+
+
             % Plot Session-Montage Comparison
             % For sessions involving multiple runs, Use plot montage to compare the runs for any potential troubleshooting
-            if(size(ses_funcs, 1) > 1 & ~exist([subs{s}, '_', sess{i}, '_run-comparison-plot.fig'], 'file'))
-                ses_objs=create_fmridat_fromdir(ses_funcs); % This command takes a long time remotely. Best to run this directly on the cluster.
+            if(size(ses_funcs, 1) > 1 & ~exist([subs{sub}, '_', sessions{ses}, '_run-comparison-plot.fig'], 'file'))
+                % ses_objs=create_fmridat_fromdir(ses_funcs); % This command takes a long time remotely. Best to run this directly on the cluster.
+                ses_objs=fmri_data(ses_funcs{1});
+                fmri_data('\\dartfs-hpc\rc\lab\C\CANlab\labdata\data\WASABI\derivatives\canlab_firstlvl\sub-SID000002\ses-01\func\run-01\sub-SID000002_ses-01_task-movemap_acq-mb8_run-01_space-MNI152NLin2009cAsym_desc-preproc_bold.nii.gz')
                 % Hopefully this is now implemented into fmri_data/plot()
                 
                 if size(ses_objs.image_names, 1) > 1
                     ses_objs=id_images_per_session(ses_objs);
                     plot(ses_objs, 'montages', 'noorthviews', 'nooutliers') % Would be better if we can relabel these?
                     fig = gcf;
-                    exportgraphics(fig, fullfile(firstlvl_derivdir, subs{s}, sess{i},[subs{s}, '_', sess{i}, '_run-comparison-plot.png']));
-                    savefig(fullfile(firstlvl_derivdir, subs{s}, sess{i}, [subs{s}, '_', sess{i}, '_run-comparison-plot.fig']));
+                    exportgraphics(fig, fullfile(firstlvl_derivdir, subs{sub}, sessions{ses},[subs{sub}, '_', sessions{ses}, '_run-comparison-plot.png']));
+                    savefig(fullfile(firstlvl_derivdir, subs{sub}, sessions{ses}, [subs{sub}, '_', sessions{ses}, '_run-comparison-plot.fig']));
                 end
             end
             
@@ -101,7 +137,7 @@ function WASABI_genfirstlvlDSGNs(firstlvl_derivdir, bidsroot)
             
             % DSGN_process_job(tasks)
             for t = 1:numel(tasks)
-                funcs=dir(fullfile(firstlvl_derivdir, subs{s}, sess{i},'func', '**', ['*',tasks{t},'*']));
+                funcs=dir(fullfile(firstlvl_derivdir, subs{sub}, sessions{ses},'func', '**', ['*',tasks{t},'*']));
                 if ~isempty(funcs) % If there are scans the pertain to the task
                     % 1. Generate a DSGN structure for that task and make a folder to
                     % house the .mat file it will reside in.
@@ -134,8 +170,8 @@ function WASABI_genfirstlvlDSGNs(firstlvl_derivdir, bidsroot)
                     % 4. Save all your files
                     save(fullfile(DSGN.modeldir,[tasks{t},'DSGN.mat']),'DSGN');
                     save([tasks{t}, '_objs'], [tasks{t}, '_objs'], '-v7.3');
-                    savefig(fig, [subs{s}, '_', sess{i}, '_', tasks{t},'_run-comparison-plot.fig']);
-                    exportgraphics(fig, [subs{s}, '_', sess{i}, '_', tasks{t},'_run-comparison-plot.png']);
+                    savefig(fig, [subs{sub}, '_', sessions{ses}, '_', tasks{t},'_run-comparison-plot.fig']);
+                    exportgraphics(fig, [subs{sub}, '_', sessions{ses}, '_', tasks{t},'_run-comparison-plot.png']);
             
                 else
                     % If there is no matching task, then display this message:
