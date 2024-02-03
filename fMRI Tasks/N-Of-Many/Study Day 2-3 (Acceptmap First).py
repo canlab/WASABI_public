cf64d3bc51754378dc32178cf75797a222835c45
Michael-Sun <msun@dartmouth.edu>
2024-02-03 15:57:33 -0500

    sub-SID002742 acceptmap experience distractmap session 3

--- fMRI Tasks/N-Of-Many/Study Day 2-3 (Acceptmap First).py
+++ fMRI Tasks/N-Of-Many/Study Day 2-3 (Acceptmap First).py
@@ -2,7 +2,7 @@
 
 import os
 
-os.system('python "C:\\Users\\Dartmouth\\Documents\\GitHub\\canlab\\WASABI_public\\fMRI Tasks\\N-Of-Many\\WASABI acceptmap\\WASABI_AcceptMap_v3.0.0.py"')
+os.system('python "C:\\Users\\Dartmouth\\Documents\\GitHub\\canlab\\WASABI_public\\fMRI Tasks\\N-Of-Many\\WASABI acceptmap\\WASABI_AcceptMap_v3.0.1.py"')
 os.system('python "C:\\Users\\Dartmouth\\Documents\\GitHub\\canlab\\WASABI_public\\fMRI Tasks\\N-Of-Many\\WASABI medMap\\WASABI_hyperalignment.py"')
 os.system('python "C:\\Users\\Dartmouth\\Documents\\GitHub\\canlab\\WASABI_public\\fMRI Tasks\\N-Of-Many\\WASABI distractmap\\WASABI_distractmap_v4.0.2.py"')
 os.system('python "C:\\Users\\Dartmouth\\Documents\\GitHub\\canlab\\WASABI_public\\fMRI Tasks\\N-Of-Many\\WASABI pinellocalizer\\pinelfMRI.py"')
