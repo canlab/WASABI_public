6ebb9caec197cb8b800816c35668bd4a9dc177dc
Michael-Sun <msun@dartmouth.edu>
2024-02-26 16:36:59 -0500

    New Rating Training Presentation for Ses-1 BodyCalibration

--- fMRI Tasks/N-Of-Many/Study Day 1.py
+++ fMRI Tasks/N-Of-Many/Study Day 1.py
@@ -1,5 +1,5 @@
 # == WASABI MedMap==
 import os
 
-os.system('python "C:\\Users\\Dartmouth\\Documents\\GitHub\\canlab\\WASABI_public\\fMRI Tasks\\N-Of-Many\\WASABI bodyCalibration\\WASABI_bodyCalibration_1.0.4.py"')
+os.system('python "C:\\Users\\Dartmouth\\Documents\\GitHub\\canlab\\WASABI_public\\fMRI Tasks\\N-Of-Many\\WASABI bodyCalibration\\WASABI_bodyCalibration_1.0.5.py"')
 os.system('python "C:\\Users\\Dartmouth\\Documents\\GitHub\\canlab\\WASABI_public\\fMRI Tasks\\N-Of-Many\\WASABI pinellocalizer\\pinelfMRI.py"')
