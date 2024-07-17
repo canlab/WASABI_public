4002f561bcd188c01719225d0e7885290aadd5e2
Michael-Sun <msun@dartmouth.edu>
2024-07-16 21:55:32 -0400

    sub-SID003000_ses-01 bodycalibration, pinel, t1, script had syntax error that was corrected, medoc stopped for the last quarter run 8 abs

--- fMRI Tasks/N-Of-Many/WASABI bodyCalibration/WASABI_bodyCalibration_1.0.5.py
+++ fMRI Tasks/N-Of-Many/WASABI bodyCalibration/WASABI_bodyCalibration_1.0.5.py
@@ -516,7 +516,7 @@ if bids_df.loc[(bids_df['body_site']=='Chest') & (bids_df['repetition']!=1) & (b
     end_msg="Thank you for your participation. \n\n\nUnfortunately you don't qualify for the continuation of this study. Experimenter please press [e]."
     averaged_df['Eligible']=-2
 
-eif averaged_df['leftleg_ht']<47 | averaged_df['chest_ht']<47:
+elif averaged_df['leftleg_ht']<47 | averaged_df['chest_ht']<47:
     end_msg="Thank you for your participation. \n\n\nUnfortunately you don't qualify for the continuation of this study. Experimenter please press [e]."
     averaged_df['Eligible']=-3
 
