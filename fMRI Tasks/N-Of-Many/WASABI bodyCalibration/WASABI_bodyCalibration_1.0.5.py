833b2335db2ccb6b956c221d0698266977e77b6b
Michael-Sun <msun@dartmouth.edu>
2024-09-01 19:59:06 -0400

    sub-SID003012_ses-03 acceptmap experience, hyperlaignment, distractmap-pinel

--- fMRI Tasks/N-Of-Many/WASABI bodyCalibration/WASABI_bodyCalibration_1.0.5.py
+++ fMRI Tasks/N-Of-Many/WASABI bodyCalibration/WASABI_bodyCalibration_1.0.5.py
@@ -1,4 +1,4 @@
-# -*- coding: utf-8 -*-
+# -*-coding: utf-8 -*-
 """
 This experiment was created using PsychoPy3 Experiment Builder (v2020.2.10),
     on January 28, 2021, at 23:04
@@ -176,7 +176,7 @@ if expInfo['body sites']=="":
     # 1. UNCOMMENT THE LINE BELOW,
     # 2. FILL IN THE FIRST ELEMENTS WITH BODYSITES ALREADY RUN, 
     # 3. AND THEN TYPE IN THE RUN YOU LEFT OFF ON IN THE DIALOGUE BOX UPON RE-RUNNING THIS TASK:!
-    bodySites = ["Right Leg","Left Face", "Right Face","Left Arm", "Right Arm", "Left Leg", "Chest", "Abdomen"]
+    #bodySites = ["Right Leg","Left Face", "Right Face","Left Arm", "Right Arm", "Left Leg", "Chest", "Abdomen"]
     
 else:
     bodySites=list(expInfo['body sites'].split(", "))
